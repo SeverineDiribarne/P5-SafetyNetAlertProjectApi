ec335801c41c7bcfdc0fda1a586ccc3e6bf804c9
sev291177 <sedi.at.home@gmail.com>
2021-10-18 12:14:13 +0200

    feature/Tests :  Tests on the getMapping and tests on the model class

---
+++ src/test/java/com/safetynetalert/alert/model/PersonTest.java
@@ -0,0 +1,42 @@
+package com.safetynetalert.alert.model;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+
+	Person person = new Person(); 
+	
+	/**
+	 * Create a new person for test
+	 * @return Person to add
+	 */
+	public Person creationANewPerson() {
+
+		// creation new person to add
+		Person personToAdd = new Person();
+		personToAdd.setId(27);
+		personToAdd.setFirstName("Erwan");
+		personToAdd.setLastName("Doe");
+		personToAdd.setBirthDate("01/26/2016");
+
+		return personToAdd;	
+	}
+	
+	/**
+	 * Test to verify Age Calculation method
+	 */
+	@Test
+	public void TestToVerifyTheAgeCalculationMethod() {
+		//TODO Method to complete
+		//GIVEN
+		Person erwan = creationANewPerson();
+		
+		//WHEN
+		Long age = erwan.ageCalculation();
+		
+		//THEN
+		assert(age.compareTo((long) 5)==0);
+	}
+}
